d9363956c85abf3327d275001859713b0a2cfbed
root <root@DESKTOP-UQ5BUJT.localdomain>
2021-08-24 12:01:57 -0300

    proyecto de rick and morty

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Rick and Morty</h1>
+    
+    <FilterByStatus/>
+    <FilterByName />
+    <ListCharacters/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListCharacters from '@/components/ListCharacters'
+import FilterByStatus from '@/components/FilterByStatus'
+import FilterByName from '@/components/FilterByName'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListCharacters,
+    FilterByStatus,
+    FilterByName
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  :root{
+    --background-body:#24282f;    
+    --background-card:#0c29bb;
+    --text-white:#ffffff;
+    --text-gray:#c0c0c0;
+    --text-orange:#ff9800;
+
+  }
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0px;
+  }
+  body{
+    background-color: var(--background-body);
+    color:var(--text-white);
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+  }
+  h1{
+    margin-bottom: 3rem;
+    text-align: center;
+  }
+  .container{
+    width: 985px;
+    max-width: 90%;
+    margin: 5rem auto
+  }
+  img{
+    width: 100%;
+  }
 </style>
